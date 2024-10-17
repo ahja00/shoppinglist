1dfa502e09d7e45c02802e65025d209682b51604
ahja00 <t2ahja00@students.oamk.fi>
2024-10-17 21:33:59 +0300

    Update Config.js

--- firebase/Config.js
+++ firebase/Config.js
@@ -2,13 +2,7 @@ import { getFirestore, collection, addDoc, serverTimestamp, getDocs, deleteDoc, 
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAGjmrRo6k60qESYjoEJRXM-TrC6AIf4oM",
-  authDomain: "ostoslista-60845.firebaseapp.com",
-  projectId: "ostoslista-60845",
-  storageBucket: "ostoslista-60845.appspot.com",
-  messagingSenderId: "944164958490",
-  appId: "1:944164958490:web:efbdc2eb2bb0f6056bbca3",
-  measurementId: "G-ML1B4RHHYZ"
+
 };
 
 
